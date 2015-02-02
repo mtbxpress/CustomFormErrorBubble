39eb64907e20f8aedf5b207ab70a72a4d9e69780
byverdu <byverdu@gmail.com>
2015-02-02 18:53:04 +0000

    solved problem multiple checkboxes

--- jsVersion.js
+++ jsVersion.js
@@ -4,7 +4,41 @@
 window.addEventListener('DOMContentLoaded',function(){
 
 	(function(){
-		var form = document.querySelector('form');
+
+		// adding the required attribute for multiple check boxes
+		var deleteRequiredAttr = function (){
+
+			var i, 
+					thisCount = document.querySelectorAll('.options:checked').length;
+
+			if(thisCount > 0){
+				for(i=0;i<allCheckBox.length;i++){
+					allCheckBox[i].removeAttribute('required');
+				}
+			} else {
+				for(i=0;i<allCheckBox.length;i++){
+					allCheckBox[i].setAttribute('required','required');
+				}
+			}
+		};
+
+		var allCheckBox = document.querySelectorAll('.options'),
+				noJS = document.querySelector('.noJS');
+
+	 	noJS.style.display = 'none';
+
+	 	for(var i =0;i<allCheckBox.length;i++){
+	 		console.log(allCheckBox[i]);
+	 		allCheckBox[i].setAttribute('required','required');
+	 		allCheckBox[i].addEventListener('change',deleteRequiredAttr);
+	 	}
+
+	 	// end multiple check boxes setting
+
+	 	// Custom form validation
+
+		var form = document.querySelector('form'),
+				button = form.querySelector('button');
 
 		// Support Safari and Android browser—each of which do not prevent
     // form submissions by default
@@ -20,8 +54,7 @@ window.addEventListener('DOMContentLoaded',function(){
 	    event.preventDefault();
 	  },true);
 
-	  var button = form.querySelector('button');
-
+	  
 	  // Adding the new behaviour to the DOM 
 	  button.addEventListener('click', function(){
 
@@ -30,36 +63,28 @@ window.addEventListener('DOMContentLoaded',function(){
 			
 			for(var i = 0; i < invalid.length;i++){
 
-				// setting the custom message if input willValidate
+				// setting the custom behavior if element willValidate
 				if(invalid[i].willValidate){
 
-					// div for the error messages
+					// create div for the error messages
 					var error = document.createElement('div');
 					// Targeting the parent on the input 
 					var label = invalid[i].parentNode;
-
-					console.log(label);
 
 					// Adding the classes to the div
 					error.className = 'error';
 					// Setting the innerHtml with the 
 					// validationMessage property for each error
 					error.textContent = invalid[i].validationMessage;
-
+					// adding the new element
 					label.insertBefore(error,invalid[i].nextSibling);
-
 				}
-
-
-				
 			}
-
-
 
 			// Removing the actual error messages
 			window.setTimeout(function(){
 
-				var allErrors =document.querySelectorAll('.error');
+				var allErrors = document.querySelectorAll('.error');
 
 				for(var i =0;i<allErrors.length;i++){
 
@@ -67,6 +92,8 @@ window.addEventListener('DOMContentLoaded',function(){
 				}
 			},5000);
 		});
+
+
 	})();
 
 });
