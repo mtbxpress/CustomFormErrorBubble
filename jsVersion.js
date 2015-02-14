2cd2895a219c443127888727d37db35b1e3a8958
byverdu <byverdu@gmail.com>
2015-02-14 19:54:56 +0000

    code refactored

--- jsVersion.js
+++ jsVersion.js
@@ -4,46 +4,7 @@
 window.addEventListener('DOMContentLoaded', function() {
 
   (function() {
-
-    // adding the required attribute for multiple check boxes
-    function deleteRequiredAttr() {
-
-      var i,
-        thisCount = document.querySelectorAll('.options:checked').length;
-
-      if (thisCount > 0) {
-        for (i = 0; i < allCheckBox.length; i++) {
-          allCheckBox[i].removeAttribute('required');
-        }
-      } else {
-        for (i = 0; i < allCheckBox.length; i++) {
-          allCheckBox[i].setAttribute('required', 'required');
-        }
-      }
-    }
-
-    var i,
-      forms,
-      allCheckBox = document.querySelectorAll('.options'),
-      noJS = document.querySelector('.noJS');
-
-    noJS.style.display = 'none';
-
-    for (i = 0; i < allCheckBox.length; i++) {
-      allCheckBox[i].setAttribute('required', 'required');
-      allCheckBox[i].addEventListener('change', deleteRequiredAttr);
-    }
-
-    // end multiple check boxes setting
-
     // Custom form validation
-
-    forms = document.querySelectorAll('form');
-
-    for (i = 0; i < forms.length; i++) {
-      changeFormUI(forms[i]);
-    }
-
     function changeFormUI(form) {
 
       // The 'invalid' event is the one that triggers the
@@ -84,14 +45,10 @@ window.addEventListener('DOMContentLoaded', function() {
             // create div for the error messages
             error = document.createElement('div');
             error.className = 'error';
-
-            // Targeting the input's parent 
-            label = invalid[i].parentNode;
-
-            // Setting the innerHtml with the 
-            // validationMessage property for each error
             error.textContent = invalid[i].validationMessage;
-            // adding the new element
+
+            // Targeting the input's parent and adding the new element
+            label = invalid[i].parentNode;
             label.insertBefore(error, invalid[i].nextSibling);
           }
         }
@@ -110,6 +67,45 @@ window.addEventListener('DOMContentLoaded', function() {
           arr[0].focus();
         }
       });
+    }
+
+    // adding the required attribute for multiple check boxes
+    function deleteRequiredAttr() {
+
+      var i,
+        thisCount = document.querySelectorAll('.options:checked').length;
+
+      if (thisCount > 0) {
+        for (i = 0; i < allCheckBox.length; i++) {
+          allCheckBox[i].removeAttribute('required');
+        }
+      } else {
+        for (i = 0; i < allCheckBox.length; i++) {
+          allCheckBox[i].setAttribute('required', 'required');
+        }
+      }
+    }
+
+    var i,
+      forms,
+      allCheckBox = document.querySelectorAll('.options'),
+      noJS = document.querySelector('.noJS');
+
+    noJS.style.display = 'none';
+
+    for (i = 0; i < allCheckBox.length; i++) {
+      allCheckBox[i].setAttribute('required', 'required');
+      allCheckBox[i].addEventListener('change', deleteRequiredAttr);
+    }
+
+    // end multiple check boxes setting
+
+    // Change behaviour for all the forms     
+
+    forms = document.querySelectorAll('form');
+
+    for (i = 0; i < forms.length; i++) {
+      changeFormUI(forms[i]);
     }
 
   })();
